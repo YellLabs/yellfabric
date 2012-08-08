ac0eac18e0a5097a74dc4fb27180cfc69b717cd9
Dan Carley <dan.carley@gmail.com>
2012-08-08 12:15:16 +0100

    Replace liquibase.render_settings_template
    No need to template the Liquibase change files. Simplifies code.

--- liquibase.py
+++ liquibase.py
@@ -2,23 +2,27 @@ import glob
 import os.path
 import sys
 import tempfile
+import shutil
 
 from fabric.api import local, env, sudo, runs_once, require, run
 from fabric.contrib.project import rsync_project
 from fabric.context_managers import cd
 
 from operations import rsync_from_local
-from utils import template_context, template_to_file
 
 def run_script():
-    render_settings_template()
     require("sudo_user")
     require("jdbc_url", "jdbc_username", "jdbc_password", "changelog_filename")
 
-    remote_dir = sudo('mktemp -d', user = env.sudo_user)
-    rsync_from_local(remote_dir)
+    local_tempdir = tempfile.mkdtemp()
+    local("tar -C'%s' -xzf '%s'" %
+        (local_tempdir, env.db_script_archive))
+    env.tempdir = local_tempdir
 
-    with cd('%s/processed-liquibase/changelog' % (remote_dir)):
+    remote_tempdir = sudo('mktemp -d', user = env.sudo_user)
+    rsync_from_local(remote_tempdir)
+
+    with cd(os.path.join(remote_tempdir, 'liquibase', 'changelog')):
         sudo("sh /usr/bin/liquibase" +
             " --driver=com.mysql.jdbc.Driver" +
             " --classpath=/usr/share/java/mysql-connector-java.jar" +
@@ -28,35 +32,10 @@ def run_script():
             " --changeLogFile=%s" % (env.changelog_filename) +
             " update", user = env.sudo_user)
 
-    sudo('rm -rf %s' % (remote_dir), user = env.sudo_user)
+    sudo('rm -rf %s' % (remote_tempdir), user = env.sudo_user)
+    shutil.rmtree(local_tempdir)
 
 def setup_paths():
     require("java_root", "java_conf", "java_log", "project_name")
 
     env.db_script_archive = "%s-liquibase.tar.gz" % env.project_name
-
-@runs_once
-def render_settings_template():
-    tempdir = tempfile.mkdtemp()
-    env.project_path = tempdir
-    local("tar -C'%s' -xzf '%s'" % (tempdir, env.db_script_archive))
-
-    liquibase_source_dir = os.path.join(tempdir, 'liquibase')
-    liquibase_target_dir = os.path.join(tempdir, 'processed-liquibase')
-
-    os.mkdir(liquibase_target_dir)
-    context = template_context(env.settings_vars)
-
-    for (root, dirs, files) in os.walk(liquibase_source_dir):
-        dirs[:] = [os.path.join(root, d) for d in dirs]
-        files = [os.path.join(root, f) for f in files]
-
-        for source_file in files:
-            target_file = source_file.replace(liquibase_source_dir, liquibase_target_dir)
-            if not (os.path.exists(os.path.dirname(target_file))):
-                os.makedirs(os.path.dirname(target_file))
-            template_to_file(source_file, target_file, context)
-
-    env.deploy_db_script_dir = liquibase_target_dir
-    env.tempdir = tempdir
-
