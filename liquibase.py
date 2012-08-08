38a515ef340131f49f398f44fffbc6a279904a02
Dan Carley <dan.carley@gmail.com>
2012-08-08 12:26:27 +0100

    Remove use of sudo in liquibase
    There aren't any commands that require privileges here.
    
    Also means that we can use normal rsync_project instead of our wrapper
    rsync_from_local for sudo.

--- liquibase.py
+++ liquibase.py
@@ -4,35 +4,35 @@ import sys
 import tempfile
 import shutil
 
-from fabric.api import local, env, sudo, runs_once, require, run
+from fabric.api import local, env, runs_once, require, run
 from fabric.contrib.project import rsync_project
 from fabric.context_managers import cd
 
-from operations import rsync_from_local
 
 def run_script():
-    require("sudo_user")
     require("jdbc_url", "jdbc_username", "jdbc_password", "changelog_filename")
 
     local_tempdir = tempfile.mkdtemp()
     local("tar -C'%s' -xzf '%s'" %
         (local_tempdir, env.db_script_archive))
-    env.tempdir = local_tempdir
 
-    remote_tempdir = sudo('mktemp -d', user = env.sudo_user)
-    rsync_from_local(remote_tempdir)
+    remote_tempdir = run('mktemp -d')
+    rsync_project(
+        local_dir="%s/" % local_tempdir,
+        remote_dir="%s/" % remote_tempdir,
+        delete=True)
 
     with cd(os.path.join(remote_tempdir, 'liquibase', 'changelog')):
-        sudo("sh /usr/bin/liquibase" +
+        run("sh /usr/bin/liquibase" +
             " --driver=com.mysql.jdbc.Driver" +
             " --classpath=/usr/share/java/mysql-connector-java.jar" +
             " --url=%s" % (env.jdbc_url) +
             " --username=%s" % (env.jdbc_username) +
             " --password=%s" % (env.jdbc_password) +
             " --changeLogFile=%s" % (env.changelog_filename) +
-            " update", user = env.sudo_user)
+            " update")
 
-    sudo('rm -rf %s' % (remote_tempdir), user = env.sudo_user)
+    run('rm -rf %s' % (remote_tempdir))
     shutil.rmtree(local_tempdir)
 
 def setup_paths():
